35be51398cf396d486b5e7353053a952ddd03123
RodolfoBaez <rodolfobaez10463@gmail.com>
2020-01-13 18:58:01 +0000

    nothing

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
@@ -9,3 +13,47 @@ var currentScrollPos = window.pageYOffset;
   prevScrollpos = currentScrollPos;
 }
 
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("donation").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("donation").style.width = "0%";
+}
+
+
+
+
+
+// donation POPUP
+var modal = document.getElementById("myModal2");
+
+var btn = document.getElementById("myBtn2");
+
+var span = document.getElementsByClassName("close2")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
